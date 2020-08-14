6ddbfaeee08fe96bdbda6367af43d0e74e807f37
Bipin Mathew <bipinmathew@gmail.com>
2020-08-14 13:38:53 -0400

    First cut of CSV web app.

---
+++ run.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import render_template, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+from markupsafe import escape
+import os
+import json
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './uploads/' 
+
+ALLOWED_EXTENSIONS = {'csv'}
+def allowed_file(filename):
+  return '.' in filename and \
+         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/")
+def run():
+  return render_template("index.html")
+
+
+@app.route("/api/csv", methods=['POST'])
+def csv_put():
+  if request.method == 'POST':
+      # check if the post request has the file part
+      if 'file' not in request.files:
+          flash('No file part')
+          return redirect(request.url)
+      file = request.files['file']
+      # if user does not select file, browser also
+      # submit an empty part without filename
+      if file.filename == '':
+          return {'status':'failed'},400
+      if file and allowed_file(file.filename):
+          filename = secure_filename(file.filename)
+          file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+          return {'status':'finished'},200
+
+
+@app.route("/api/csv", methods=['GET'])
+def csv_list():
+	return {'files': os.listdir(app.config['UPLOAD_FOLDER']) } ,200
+ 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
