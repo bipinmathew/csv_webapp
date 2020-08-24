a1695fddcda351432a8e0c0cc92cde1aa689fd63
Bipin Mathew <bipinmathew@gmail.com>
2020-08-23 22:07:09 -0400

    Start making nice grids and bring in pandas to do statistics.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var Vue = require("vue");
 var axios = require("axios");
 var $ = require("jquery");
+window.Grid = require("gridjs");
 
 window.app = Vue.createApp({
 	delimiters: ['[[',']]'],
@@ -9,46 +10,50 @@ window.app = Vue.createApp({
 			files: [] 
 		}
 	},
+  methods: {
+    uploadHandler: function(){
+      $.ajax({
+        url: '/api/csv',
+        type: 'POST',
+        data: new FormData($('#upload_form')[0]),
+
+        cache: false,
+        contentType: false,
+        processData: false,
+
+        xhr: function () {
+          console.log($('#upload_form'));
+          var myXhr = $.ajaxSettings.xhr();
+          if (myXhr.upload) {
+            // For handling the progress of the upload
+            myXhr.upload.addEventListener('progress', function (e) {
+              if (e.lengthComputable) {
+                $('progress').attr({
+                  value: e.loaded,
+                  max: e.total,
+                });
+              }
+            }, false);
+          }
+          return myXhr;
+        },
+      });
+    },
+    updateFile: function(){
+      var file = this.files[0];
+    },
+    handlePreview: function(args){
+      console.log(args);
+      axios.get('/api/csv/'+args)
+        .then(response =>(Grid.Grid(response.data.csv).render(document.getElementById('#csv_preview'))));
+    }
+  },
 	mounted: function(){
+    $('progress').attr({
+      value: 0,
+      max: 100,
+    });
 		axios.get('/api/csv')
 			.then(response => (this.files = response.data.files));
 	}
-
-})
-
-$('progress').attr({
-  value: 0,
-  max: 100,
-});
-
-$(':file').on('change', function () {
-  var file = this.files[0];
-});
-$(':button').on('click', function () {
-  $.ajax({
-    url: '/api/csv',
-    type: 'POST',
-    data: new FormData($('#upload_form')[0]),
-
-    cache: false,
-    contentType: false,
-    processData: false,
-
-    xhr: function () {
-      console.log($('#upload_form'));
-      var myXhr = $.ajaxSettings.xhr();
-      if (myXhr.upload) {
-        // For handling the progress of the upload
-        myXhr.upload.addEventListener('progress', function (e) {
-          if (e.lengthComputable) {
-            $('progress').attr({
-              value: e.loaded,
-              max: e.total,
-            });
-          }
-        }, false);
-      }
-      return myXhr;
-    },
-  });
 });
--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from werkzeug.utils import secure_filename
 from markupsafe import escape
 import os
 import json
+import pandas as pd
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './uploads/' 
@@ -39,6 +40,14 @@ def csv_put():
 @app.route("/api/csv", methods=['GET'])
 def csv_list():
 	return {'files': os.listdir(app.config['UPLOAD_FOLDER']) } ,200
+
+
+@app.route("/api/csv/<filename>", methods=['GET'])
+def csv_preview(filename):
+  df = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+  df = df.head(10)
+  parsed = df.to_json(orient="split")
+  return {'csv': parsed},200
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
